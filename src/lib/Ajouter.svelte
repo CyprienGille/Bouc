c195647034cce0f0700fa973fbbff5ab1f122cf5
Cyprien <cyprien.gille@orange.fr>
2023-10-05 21:42:23 +0200

    Update dependencies

--- src/lib/Ajouter.svelte
+++ src/lib/Ajouter.svelte
@@ -108,7 +108,7 @@
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="input-year"
                 type="text"
-                placeholder="1682"
+                placeholder="1665"
               />
             </div>
           </div>
